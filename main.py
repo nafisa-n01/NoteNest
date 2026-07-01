daed48d08c3c28eba5c64db3374f12cc74e5a726
Nafisa Nawyar <nafisan7101@gmail.com>
2026-07-01 21:12:44 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
+
+from kivymd.app import MDApp
+
+from screens.home_screen import HomeScreen
+from screens.settings_screen import SettingsScreen
+
+
+class NoteNestApp(MDApp):
+
+    def build(self):
+        self.title = "NoteNest"
+
+        Builder.load_file("app.kv")
+
+        sm = ScreenManager()
+
+        sm.add_widget(HomeScreen(name="home"))
+        sm.add_widget(SettingsScreen(name="settings"))
+
+        sm.current = "home"
+
+        return sm
+
+
+if __name__ == "__main__":
+    NoteNestApp().run()
+
+
+
+'''
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
@@ -17,3 +49,4 @@ class NotebookApp(MDApp):
 
 if __name__ == '__main__':
     NotebookApp().run()
+'''
