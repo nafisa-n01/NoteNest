8b28dd5e0b4b1821979d54f440d86f93841064ca
nafisa-n01 <nafisan7101@gmail.com>
2026-07-26 19:38:19 +0600

    formatting toolbar previous changes removed

--- screens/editor/formatting_toolbar.py
+++ screens/editor/formatting_toolbar.py
@@ -8,33 +8,11 @@
 from kivymd.uix.card import MDCard
 from kivy.properties import BooleanProperty
 
-from theme.theme_manager import theme_manager
-from theme.palettes import CARD_SECONDARY, TEXT_PRIMARY
-
-_ICON_BUTTON_IDS = [
-    "undo_button", "redo_button",
-    "bold_button", "italic_button", "underline_button", "highlight_button", "link_button",
-    "align_left_button", "align_center_button", "align_right_button",
-    "font_decrease_button", "font_increase_button", "font_cycle_button",
-]
-
 class FormattingToolbar(MDCard):
     # Set from Python whenever the toolbar is moved -- the KV rule
     # below uses this to switch between its two visual styles (flat
     # and docked, vs rounded/elevated and floating).
     is_compact = BooleanProperty(False)
-
-    def apply_theme(self):
-        if self.is_compact:
-            self.md_bg_color = theme_manager.get_color(CARD_SECONDARY)
-        else:
-            self.md_bg_color = (0, 0, 0, 0)
-
-        icon_color = theme_manager.get_color(TEXT_PRIMARY)
-        for button_id in _ICON_BUTTON_IDS:
-            button = self.ids.get(button_id)
-            if button is not None:
-                button.icon_color = icon_color
 
     def on_is_compact(self, instance, value):
         self.apply_theme()
